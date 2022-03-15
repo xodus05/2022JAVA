1d4807ccf8840bb04c662be8d2b55da65717e75a
Mirim <Mirim@DESKTOP-95IR0SK>
2022-03-15 11:02:02 +0900

    Class

---
+++ src/CircleArray01/CircleArray.java
@@ -0,0 +1,13 @@
+package CircleArray01;
+
+public class CircleArray {
+	public static void main(String[] args) {
+		Circle [] c;
+		c = new Circle[5];
+		
+		for(int i = 0; i<c.length; i++)
+			c[i] = new Circle(i);
+		for(int i = 0; i <c.length; i++)
+			System.out.println((int)(c[i].getArea()) + " ");
+	}
+}
