c616580ab3af60c94fa82b276ece0f628552eb10
xodus05 <82927278+xodus05@users.noreply.github.com>
2022-03-15 11:38:37 +0900

    배열객체

--- src/CircleArray02/BookArray.java
+++ src/CircleArray02/BookArray.java
@@ -7,13 +7,16 @@ public class BookArray {
 		Scanner scanner = new Scanner(System.in);
 		for (int i = 0; i < book.length; i++) {
 			System.out.print("제목>>");
+			
 			String title = scanner.nextLine();
 			System.out.print("저자>>");
+			
 			String author = scanner.nextLine();
 			book[i] = new Book(title, author); // 배열 원소 객체 생성
 		}
 		for (int i = 0; i < book.length; i++)
 			System.out.print("(" + book[i].title + ", " + book[i].author + ")");
+		
 		scanner.close();
 	}
 }
