f054f0502c7a78e7b7cef2d21aeea85dbd8e0fc9
xodus05 <82927278+xodus05@users.noreply.github.com>
2022-03-15 12:12:51 +0900

    Static

--- src/CircleArray03/AccessEx.java
+++ src/CircleArray03/AccessEx.java
@@ -4,7 +4,7 @@ public class AccessEx {
 	public static void main(String args[]) {
 		Sample aClass = new Sample();
 		aClass.a = 10;
-		aClass.b = 10;
+		// aClass.b = 10; 오류 발생
 		aClass.c = 10;
 	}
 }
